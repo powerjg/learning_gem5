1bf2a61f5564f99e3479a7ffaeeba40f04388cd0
Mingyuan <xiang_my@outlook.com>
2019-06-11 21:42:02 -0500

    Fix typos which will cause compile errors

--- _static/scripts/part2/parameters/hello_object.hh
+++ _static/scripts/part2/parameters/hello_object.hh
@@ -45,7 +45,7 @@ class HelloObject : public SimObject
     EventWrapper<HelloObject, &HelloObject::processEvent> event;
 
     /// Pointer to the corresponding GoodbyeObject. Set via Python
-    GoodbyeObject& goodbye;
+    GoodbyeObject* goodbye;
 
     /// The name of this object in the Python config file
     std::string myName;
